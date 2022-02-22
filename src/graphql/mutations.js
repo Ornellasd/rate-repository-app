64b45535edc1e4e5c2dbf26864aba8c89ebb0dc8
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-02-22 16:43:07 +0100

    intialize apollo client with access token usage

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,13 +1,5 @@
 import { gql } from '@apollo/client';
 
-// export const AUTHENTICATE_USER = gql`
-//   mutation {
-//     authenticate(credentials: { $username: String!, $password: String! }) {
-//       accessToken
-//     }
-//   }
-// `;
-
 export const AUTHENTICATE_USER = gql`
   mutation authenticateUser($credentials: AuthenticateInput!) {
     authenticate(credentials: $credentials) {
