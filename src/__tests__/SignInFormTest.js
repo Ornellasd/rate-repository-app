4157bc4d14cd09b078db07373fb5ae3adb0ae213
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-03-09 22:51:59 +0100

    finish 10.18 - sign in form test implemented

--- src/__tests__/SignInFormTest.js
+++ src/__tests__/SignInFormTest.js
@@ -1,15 +1,28 @@
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import SignInContainer from '../components/SignIn';
+import { SignInContainer } from '../components/SignIn';
 
 describe('SignIn', () => {
   describe('SignInContainer', () => {
     it('calls onSubmit function with correct arguments when a valid form is submitted', async () => {
       // render the SignInContainer component, fill the text inputs and press the submit button
+      const onSubmit = jest.fn();
+      
+      const { getByPlaceholderText, getByText } = render(<SignInContainer onSubmit={onSubmit} />);
 
-      const { getByPlaceHolderText } = render(<SignInContainer signIn={signInData} />);
+      fireEvent.changeText(getByPlaceholderText('Username'), 'test');
+      fireEvent.changeText(getByPlaceholderText('Password'), 'testy');
+      fireEvent.press(getByText('Sign In'));
+      
       await waitFor(() => {
         // expect the onSubmit function to have been called once and with a correct first argument
+        expect(onSubmit).toHaveBeenCalledTimes(1);
+        expect(onSubmit.mock.calls[0][0]).toEqual({
+          username: 'test',
+          password: 'testy',
+        });
+
       });
+
     });
   });
 });
