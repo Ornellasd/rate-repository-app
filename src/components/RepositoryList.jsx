0ec49e70a1e5ef47d50938979945a02c1a916f21
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-04-14 16:06:29 -0700

    initialize repofilter input box

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,32 +1,43 @@
 import { useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View, StyleSheet, TextInput } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import useRepositories from '../hooks/useRepositories';
 
+// import TextInput from './TextInput';
+
+
 import RepositoryItemContainer from './RepositoryItem';
 import ItemSeparator from './ItemSeparator';
+
+const styles = StyleSheet.create({
+  filterInputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    margin: 10,
+    padding: 8,
+    borderRadius: 4,
+
+  },
+  filterInput: {
+    flex: 1,
+  },
+  searchIcon: {
+    paddingRight: 20,
+  },
+  closeIcon: {
+    paddingLeft: 20,
+  }
+});
 
 const renderItem = ({ item }) => (
   <View>
     <RepositoryItemContainer item={item} />
   </View>
 );
-
-export const RepositoryListContainer = ({ repositories, sortPrinciple, sortChange }) => {
-  const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
-    : [];
-
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={renderItem}
-      ListHeaderComponent={() => <SortPicker sortPrinciple={sortPrinciple} sortChange={sortChange} />}
-    />
-  );
-};
 
 const SortPicker = ({ sortPrinciple, sortChange }) => {
   const handleSortChange = (sort) => {
@@ -44,6 +55,38 @@ const SortPicker = ({ sortPrinciple, sortChange }) => {
       <Picker.Item label="Highest rated repositories" value="highest" />
       <Picker.Item label="Lowest rated repositories" value="lowest" />
     </Picker>
+  );
+};
+
+const RepoFilter = () => {
+  return (
+    <View style={styles.filterInputContainer}>
+      <Icon name="search1" size={20} color="#000" style={styles.searchIcon} />
+      <TextInput style={styles.filterInput} />
+      <Icon name="close" size={20} color="#000" style={styles.closeIcon} />
+    </View>
+  );
+};
+
+const Header = ({ sortPrinciple, sortChange }) => (
+  <View>
+    <RepoFilter />
+    <SortPicker sortPrinciple={sortPrinciple} sortChange={sortChange} />
+  </View>
+);
+
+export const RepositoryListContainer = ({ repositories, sortPrinciple, sortChange }) => {
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
+  return (
+    <FlatList
+      data={repositoryNodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={renderItem}
+      ListHeaderComponent={() => <Header sortPrinciple={sortPrinciple} sortChange={sortChange} />}
+    />
   );
 };
 
